9d271d578c40ca4fad67ea207648c2e5f5d72fb7
XU PU <ptx.pluto@gmail.com>
2021-07-25 22:59:55 +0800

    frictional settings for the plane

--- Rock-Walk/rock_walk/resources/plane.py
+++ Rock-Walk/rock_walk/resources/plane.py
@@ -1,26 +1,25 @@
-import pybullet as bullet
+import pybullet
 import os
 
 
 class Plane:
     def __init__(self, client):
+        pybullet.setAdditionalSearchPath(
+            os.path.join(os.path.dirname(__file__), 'models')
+        )
         self.clientID = client
+        self.bodyID = pybullet.loadURDF(
+            'plane.urdf',
+            [0, 0, 0],
+            physicsClientId=self.clientID
+        )
+        self.textureID = pybullet.loadTexture('checker_blue.png')
+        pybullet.changeVisualShape(self.bodyID, -1, textureUniqueId=self.textureID, physicsClientId=self.clientID)
 
-        f_name1 = os.path.join(os.path.dirname(__file__), 'models/plane.urdf')
-        f_name2 = os.path.join(os.path.dirname(__file__), 'models/checker_blue.png')
+    @property
+    def lateral_friction(self):
+        return pybullet.getDynamicsInfo(self.bodyID, -1, physicsClientId=self.clientID)[1]
 
-        self.planeID = bullet.loadURDF(fileName=f_name1, basePosition=[0, 0, 0], physicsClientId=self.clientID)
-        texID = bullet.loadTexture(f_name2)
-
-        bullet.changeVisualShape(self.planeID, -1, textureUniqueId=texID, physicsClientId=client)
-
-
-    def get_ids(self):
-        return self.planeID, self.clientID
-
-
-    def get_dynamics_info(self):
-        print(bullet.getDynamicsInfo(self.planeID, -1, self.clientID))
-
-    def set_lateral_friction(self, value):
-        bullet.changeDynamics(self.planeID, -1, lateralFriction=value, physicsClientId=self.clientID)
+    @lateral_friction.setter
+    def lateral_friction(self, value):
+        pybullet.changeDynamics(self.bodyID, -1, lateralFriction=value, physicsClientId=self.clientID)
